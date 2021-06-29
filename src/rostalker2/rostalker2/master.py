9093caa35057ece4aa08d671ea01876cef128c37
urd00m <alan.linghao.wang@gmail.com>
2021-06-29 16:49:11 -0500

    Bug fixes to the loader feature of the model and added in better exception handling, readability, and usability

--- src/rostalker2/rostalker2/master.py
+++ src/rostalker2/rostalker2/master.py
@@ -5,51 +5,75 @@ import sys
 import time
 from random import random
 from rostalker2interface.srv import *
+from pathlib import Path
 
+# Only one master node can be running at anytime, or else you will cause issues 
 class Master(Node):
-	
+
 	def __init__(self):
-		super().__init__("master_node")
+		# Node creation
+		super().__init__("master_node") #TODO: Add in the ability to have multiple masters (maybe?)
+
+		# Readability
+		self.states = { #TODO: more states
+			"BUSY":1,
+			"READY":0
+		}
+
+		# Path setup
+		path = Path()
+		self.home_location = str(path.home())
+		self.module_location = self.home_location + "/ros2tests/src/OT2_Modules/"
 
 		# Service setup 
 		self.register_service = self.create_service(Register, 'register', self.handle_register)
-		self.nodes = 1
+		self.nodes = 0
 		self.nodes_list = []
 		self.register_lock = Lock()
 
 		# Get Node
-		while(len(self.nodes_list) == 0):
+		# Master doesn't do anywork until there is a worker node to do stuff with
+		self.get_logger().info("Waitting for nodes...")
+		while(len(self.nodes_list) == 0): 
+			rclpy.spin_once(self) # Allow node to register itself
 			self.get_logger().info("Waitting for nodes...")
-			time.sleep(2) # 2 seconds
-			rclpy.spin_once(self)
+			time.sleep(.5) # 2 seconds
 
 		# Initialization Complete
 		self.get_logger().info("Master initialization complete")
 
-	def load(self, name):
+	# Loads filename to a worker node
+	def load(self, name, replacement):
 
 		# Select a node
-		temp_node = self.nodes_list[int(random()*len(self.nodes_list))]
-		type = temp_node['type']
-		id = temp_node['id']
+		target_node = self.nodes_list[int(random()*len(self.nodes_list))] #TODO switch from random assignment
+		type = target_node['type'] # These will be needed to acess the service
+		id = target_node['id']
+
 		# Client setup 
-		self.load_cli = self.create_client(LoadService, "/%s/%s/load"%(type, id))
+		self.load_cli = self.create_client(LoadService, "/%s/%s/load"%(type, id)) # format of service is /{type}/{id}/{service name}
 		while not self.load_cli.wait_for_service(timeout_sec=2.0):
 			self.get_logger().info("Service not available, trying again...")
+
+		# Client ready
 		try:
-			f = open("test/"+name, "r")
+			f = open(self.module_location+name, "r") # TODO: create an actual well-known directory for modules to be placed in
 			contents = f.read()
 			f.close()
 		except Exception as e:
-			rospy.get_logger().info("Error occured: %r"%(e,))
+			self.get_logger().error("Error occured: %r"%(e,))
 		else:
-			self.get_logger().info("File %s read"%name)
+			self.get_logger().info("File %s read complete"%name)
+
+			# Create a request
 			load_request = LoadService.Request()
-			load_request.name = name
-			load_request.contents = contents
-			load_request.replace = True
+			load_request.name = name # File path: insert file name, the file path even though the same is given to the client to set up
+			load_request.contents = contents # File string contents
+			load_request.replace = replacement # If the file exists do we overwrite it?
+
+			# Call service to load module
 			self.future = self.load_cli.call_async(load_request)
-			self.get_logger().info("Futured created")
+			self.get_logger().info("Waiting for completion...")
 
 			# Waiting on future
 			rclpy.spin_until_future_complete(self, self.future)
@@ -57,31 +81,49 @@ class Master(Node):
 				try:
 					response = self.future.result()
 				except Exception as e:
-					self.get_logger().info("Error occured %r"%(e,))
-				if(response.status == 1):
+					self.get_logger().error("Error occured %r"%(e,))
+
+				# Error handling
+				if(response.status == response.ERROR):
 					self.get_logger().error("Error occured in loading at %s for file %s"%(id, name))
+				elif(response.status == response.WARNING):
+					self.get_logger().warning("Warning: File %s already exists on system %s"%(name, id))
 				else:
 					self.get_logger().info("Load succeeded")
 
-
+	# Registers a worker with the master so modules can be distrubuted
 	def handle_register(self, request, response):
 		self.register_lock.acquire()
 		response = Register.Response()
-		dict = {
-			"type":request.type,
-			"id":"O"+str(self.nodes)
-		}
-		self.nodes_list.append(dict)
-		response.status = 0
-		response.id = dict['id']
+		if(request.type == 'OT_2'):
+			dict = {
+				"type":request.type,
+				"id":"O"+str(self.nodes),
+				"state":self.states['READY']
+			}
+			self.nodes_list.append(dict)
+			self.get_logger().info("Registered ID: %s with master"%dict['id'])
+		# TODO: more types
+		else:
+			self.get_logger().error("type %s not supported at this moment"%request.type)
+			response.status = response.ERROR # Error
+			return response
+
+		response.status = response.SUCCESS
+		response.id = dict['id'] # Send back the ID to the worker
 		self.nodes += 1
 		self.register_lock.release()
 		return response
 
+	#TODO
+	def handle_destroy_worker(self, request, response):
+		pass
+
+# This is just for testing, this class can be used anywhere 
 def main(args=None):
 	rclpy.init(args=args)
 	master = Master()
-	master.load("test.py")
+	master.load("test.py", False)
 	master.destroy_node()
 	rclpy.shutdown()
 
--- src/rostalker2/rostalker2/ot2Class.py
+++ src/rostalker2/rostalker2/ot2Class.py
@@ -7,72 +7,116 @@ from rostalker2interface.srv import *
 import os
 import os.path
 from os import path
+from pathlib import Path
 
 class OT2(Node):
 
 	def __init__(self):
+
+		# Node creation
 		super().__init__("ot2") #TODO: anonymous
+
+		# Lock creation
 		self.load_lock = Lock()
 		self.run_lock = Lock()
-		
+
+		# readability
+		self.status = {
+			"ERROR":1,
+			"SUCCESS":0
+		}
+
+		# Path setup
+		path = Path()
+		self.home_location = str(path.home())
+		self.module_location = self.home_location + "/ros2tests/src/OT2_Modules/"
+
 		# Create clients
-		self.register_cli = self.create_client(Register, 'register')
+		self.register_cli = self.create_client(Register, 'register') # All master service calls will be plain, not /{type}/{id} (TODO: change to this maybe?)
 		while not self.register_cli.wait_for_service(timeout_sec=2.0):
 			self.get_logger().info("Service not available, trying again...")
 
 		# Register with master
-		self.register()
+		self.register() #TODO: need to setup retry
 
-		# Create services
+		# Create services: Have to wait until after registration this way we will have the id
 		self.load_service = self.create_service(LoadService, "/OT_2/%s/load"%self.id, self.load_handler) 
 #		self.run_service = self.create_service(TODO, "/OT_2/%s/run"%self.id, self.run_handler)
 
+	# Handles load_module service calls
 	def load_handler(self, request, response):
+		# Get lock, enter critical section
 		self.load_lock.acquire()
-		name = request.name 
+
+		# Get request information
+		name = request.name
+		file = self.module_location + name
 		contents = request.contents
-		# Error checking
-		if(path.exists(name) and request.replace == False):
-			self.get_logger().info("File %s already exists on the system and replacement is marked as false")
+
+		# Warnings
+		if(path.exists(file) and request.replace == False):
+			self.get_logger().warning("File %s already exists on the system and replacement is false, upload terminating..."%name)
 			self.load_lock.release()
-			response.status = 1 # Error
+			response.status = response.WARNING # Warning: in this context file already exists on system
 			return response
+		if(request.replace == True):
+			self.get_logger().warning("Replacement is set to True, file %s on this system will be replace with file from master"%name)
+
+		# Begin loading module
 		self.get_logger().info("Beginning load")
 		try:
-			f = open(name, "w")
+			f = open(file, "w") #TODO: add-well known directory for this to be placed in
 			f.write(contents)
 			f.close()
-			os.chmod(name, 0o777)
+			os.chmod(file, 0o777) # Exectuable permissions
 		except Exception as e:
-			self.get_logger().info("Error occured: %r"%(e,))
-			response.status = 1 # Error
+			self.get_logger().error("Error occured: %r"%(e,))
+			response.status = response.ERROR # Error
 		else:
-			self.get_logger().info("Loaded success")
-			response.status = 0 # All good
+			self.get_logger().info("File %s loaded to OT2"%name)
+			response.status = response.SUCCESS # All good
 		finally:
 			self.load_lock.release()
 			return response
 
+	# Handles run module service calls
 	def run_handler(self, request, response):
 		pass
 
 	def register(self):
-		# Obtain our count
+
+		# Create request
 		req = Register.Request()
 		req.type = "OT_2"
+
+		# Call
 		self.future = self.register_cli.call_async(req)
-		while rclpy.ok():
-			rclpy.spin_once(self)
-			if(self.future.done()):
-				try:
-					self.id = self.future.result().id
-				except Exception as e:
-					self.get_logger().info("Error occured: %r"%(e,))
-				else:
-					self.get_logger().info("Registration complete id: %s"%self.id)
-				break
-			self.get_logger().info("Waiting...")
-			time.sleep(1) # 1 second
+		self.get_logger().info("Registering with master...")
+
+		# Wait for completion
+		rclpy.spin_until_future_complete(self, self.future)
+		if(self.future.done()):
+			try:
+				# Get Response
+				response = self.future.result()
+
+				# Error checking
+				if(response.status == response.ERROR):
+					self.get_logger().error("Registration of type OT_2 failed, retrying...") #TODO: setup retry
+					return self.status['ERROR']
+
+				# All good
+				self.id = self.future.result().id # Set ID
+			except Exception as e:
+				self.get_logger().error("Error occured: %r"%(e,))
+				return self.status['ERROR']
+			else:
+				self.get_logger().info("Registration complete id: %s"%self.id)
+				return self.status['SUCCESS']
+		else: # This should never happen
+			rospy.get_logger().fatal("future failed, critical failure")
+			rospy.get_logger().fatal("Program is now terminating, PLEASE NOTE: System may be unstable")
+			sys.exit(1)
 
 def main(args=None):
 	rclpy.init(args=args)
