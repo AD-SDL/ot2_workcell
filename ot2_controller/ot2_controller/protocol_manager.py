90df965a480fe28b4465d1daecb73503b11c5ce4
urd00m <alan.linghao.wang@gmail.com>
2022-02-14 17:57:03 -0600

    regorganization

--- ot2_controller/ot2_controller/protocol_manager.py
+++ ot2_controller/ot2_controller/protocol_manager.py
@@ -7,6 +7,15 @@ import os
 import os.path
 from os import path
 
+# Others 
+from threading import Thread, Lock
+import time
+import sys
+from pathlib import Path
+import importlib.util
+from random import random
+from typing import Protocol
+
 # ROS messages and services 
 from workcell_interfaces.srv import *
 from workcell_interfaces.msg import *
@@ -30,14 +39,7 @@ from arm_client.transfer_api import _load_transfer
 from ot2_client.publish_ot2_state_api import *
 from ot2_client.publish_ot2_state_api import _update_ot2_state
 
-# Others 
-from threading import Thread, Lock
-import time
-import sys
-from pathlib import Path
-import importlib.util
-from random import random
-from typing import Protocol
+
 class OT2ProtocolManager(Node):
     def __init__(self, name):
         # Create a temporary node so we can read in parameters
