9f3f80735ef2949f818defd3f06b91396c294bf3
Dozgulbas <dozgulbas@anl.gov>
2022-02-24 20:46:51 -0600

    Executes the commands on the OT2 internal RPi

--- protocol_handling/execute_command.py
+++ protocol_handling/execute_command.py
@@ -2,8 +2,9 @@ from subprocess import PIPE, run
 
 
 def execute_command(command):
+    print("Executing the given command on the OT2")
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
-    print(result.stdout, result.stderr)
-    #print(result.stderr)
+    print(result.stdout)
+    print(result.stderr)
     return result
 
