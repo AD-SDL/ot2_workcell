d8721e4fe5d1daeeccb79e603631ebad0f2cd9f3
urd00m <alan.linghao.wang@gmail.com>
2022-02-28 14:00:48 -0600

    Build bug fixes

--- scheduler_controller/setup.py
+++ scheduler_controller/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "scheduler_manager = scheduler.schedulerManager:main"
+            "scheduler_manager = scheduler_controller.schedulerManager:main"
         ],
     },
 )
