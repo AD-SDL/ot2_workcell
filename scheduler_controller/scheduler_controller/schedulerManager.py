992b7aa7a997eadd1375bf21775134301c56add0
urd00m <alan.linghao.wang@gmail.com>
2022-03-04 13:00:38 -0600

    bug fixes

--- scheduler_controller/scheduler_controller/schedulerManager.py
+++ scheduler_controller/scheduler_controller/schedulerManager.py
@@ -203,6 +203,7 @@ class schedulerManager(Node):
 
                 # Load/Add the Protocols
                 try:
+                    self.get_logger().warn("Hit")
                     for i in range(len(next_block)):
                         if(not next_block[i].split(":")[0] == 'transfer'): # Don't send files if transfer
                             load_protocols_to_ot2(self, node, next_block[i])
