c2966f7140e0cdd8aa6bbd15e221de3d89ae694d
urd00m <alan.linghao.wang@gmail.com>
2022-03-11 15:06:00 -0600

    Bug fixes and testing

--- scheduler_controller/scheduler_controller/schedulerManager.py
+++ scheduler_controller/scheduler_controller/schedulerManager.py
@@ -162,7 +162,6 @@ class schedulerManager(Node):
 
         # return status 
         response.status = response.SUCCESS
-        self.get_logger().warn(str(response))
         return response
 
     # Helper function TODO
