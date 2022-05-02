b5631724c78c74687351fa564354b3f30c69f637
urd00m <alan.linghao.wang@gmail.com>
2022-05-02 20:24:35 +0000

    Decreased output cluttering the terminal console

--- database/protocol_handler/protocol_handling_client.py
+++ database/protocol_handler/protocol_handling_client.py
@@ -11,13 +11,12 @@ from protocol_handler.protocol_parser import *
 
 def handler(Protocol_ID): 
     path, protocol = pull_protocol(Protocol_ID)  
-    print("Protocol saved into " + path + "directory")
+    #print("Protocol saved into " + path + "directory")
     status = transfer(path)
     if(status == 1): # error 
         return "", "", 1
 
-    #protocol = "/path/to/Protocol_2022-02-18_17:13:44.py"
-    print(protocol)
+    #print(protocol)
     msg_error, msg_output, msg_errorcode = send_message_to_OT2("python3 "+ "/tmp/" + protocol.split("/")[-1])
     
     return msg_output, msg_error, msg_errorcode
@@ -28,16 +27,16 @@ def send_message_to_OT2(message):
     sock = ctx.socket(zmq.REQ)
     sock.connect("tcp://10.193.254.91:8085") # TODO: instead of 10.193.254.91 put your IP  TODO: don't hard code the IP, make it a ROS parameter
 
-    print("Starting protocol handling client...")
+    #print("Starting protocol handling client...")
     while True:
         sock.send_string(message)
-        print("Sent string: %s ..." % message)
+        #print("Sent string: %s ..." % message)
         time.sleep(1)
         msg = sock.recv_string()
         msg = msg.split('@')
         msg_output, msg_error, msg_errorcode = msg[0], msg[1], msg[2]
         if msg_output != None:
-            print("Client recived the output message from the completed protocol. Sending message to the ROS Master")
+            #print("Client recived the output message from the completed protocol. Sending message to the ROS Master")
             return msg_error, msg_output, int(msg_errorcode)
     sock.close()
 
