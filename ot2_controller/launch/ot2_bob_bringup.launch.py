08224a1b8ce9611916c87573e5a17cb1c84bbbdf
urd00m <alan.linghao.wang@gmail.com>
2022-05-10 01:28:10 +0000

    Bug detected with the full test, I think arm transfers are causing stalls

--- ot2_controller/launch/ot2_bob_bringup.launch.py
+++ ot2_controller/launch/ot2_bob_bringup.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
         output='screen',
         parameters=[{"name": "bob", "ip":"10.193.254.91", "port":"8085", "username":"nerra"}],
         emulate_tty=True,
-        arguments=['--ros-args', '--log-level', 'WARN']
+        arguments=['--ros-args', '--log-level', 'INFO']
     )
     protocol_handler = Node(
         package="ot2_controller",
@@ -18,7 +18,7 @@ def generate_launch_description():
         output='screen',
         parameters=[{"name": "bob", "ip":"10.193.254.91", "port":"8085", "username":"nerra"}],
         emulate_tty=True,
-        arguments=['--ros-args', '--log-level', 'WARN']
+        arguments=['--ros-args', '--log-level', 'INFO']
     )
     ld.add_action(ot2_controller)
     ld.add_action(protocol_handler)
